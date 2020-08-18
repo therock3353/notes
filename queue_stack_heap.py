64522642088429fd8e26f120163d9c6029597451
Pushkar <pghayal@a10networks.com>
2020-08-17 18:14:26 -0700

    heap notes

--- queue_stack_heap.py
+++ queue_stack_heap.py
@@ -33,3 +33,54 @@ while stack:
 #In python, the list itself acts as a stack.
 # s.append(element)
 # s.pop() pop() method removes the last added element simulating the stack behavior
+
+#############################################
+#############################################
+#############################################
+###         Heap                    #########
+
+#Calculate TOP-k frequent elements
+#[1,1,1,1,2,2,3] k=2 then return [1,2]
+import heapq
+def top_k(data, k):
+    top_k_elements = []
+    heap = []
+    map = {}
+    for num in data:
+        if map.get(num, None) is None:
+            map[num] = 1
+        else:
+            map[num] += 1
+
+    print map
+    for key in map:
+        heapq.heappush(heap, (-map.get(key), key))
+
+    for _ in range(k):
+        top_k_elements.append(heapq.heappop(heap)[1])
+
+    return top_k_elements
+
+#
+#  ** Python only has min-heap implemented.
+#
+#    import heapq
+#    data = []
+# 1. heapq.heappush(data, elem)
+# 2. elem = heapq.heappop(data)
+# 3. heapq.heapify([10,2,7,3]) #sort the list as heap data structure
+#
+# 4. If you push tuple (10, a), (4, b), (12, c) into heap then it is stored
+#   accordingly (4,b)->(10,a)->(12,c)
+#
+#
+data = [(10,'a'), (4, 'b'), (12, 'c')]
+heap = []
+for val in data:
+    heapq.heappush(heap, val)
+
+while heap:
+    print heapq.heappop(heap)
+# (4, 'b'),(10, 'a'),(12,'c')
+
+
