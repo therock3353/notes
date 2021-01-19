c5271cfe4e9302d83cfd94ecf264ed0d3d74d34f
therock3353 <pushkarghayal@gmail.com>
2021-01-18 18:43:30 -0800

    min heap

--- min_heap.py
+++ min_heap.py
@@ -21,10 +21,10 @@ class MinHeap(object):
     def _parent(self, i):
         return (i-1)/2
 
-    def _left_child(self, i):
+    def _left_child_ptr(self, i):
         return 2*i+1
 
-    def _right_child(self, i):
+    def _right_child_ptr(self, i):
         return 2*i+2
 
     def insert(self, e):
@@ -33,21 +33,29 @@ class MinHeap(object):
         self.last += 1
 
     def extract_min(self):
-        pass
+        if self.last == 0:
+            print "Queue is empty"
+            return
+        min_elem = self.data[0]
+        self.data[0], self.data[self.last] = self.data[self.last], self.data[0]
+        self._bubble_down()
+        self.last -= 1
+        return min_elem
 
     def _bubble_up(self, current_node_ptr):
         if current_node_ptr == 0:
             return
-        #current_node = current_node_ptr
         parent_ptr = self._parent(current_node_ptr)
         if self.data[parent_ptr] > self.data[current_node_ptr]:
             self.data[parent_ptr], self.data[current_node_ptr] = self.data[current_node_ptr], self.data[parent_ptr]
             current_node_ptr = parent_ptr
             self._bubble_up(current_node_ptr)
 
-    def _bubble_down(self):
-        pass
-
+    def _bubble_down(self, parent_ptr):
+        if self.data[parent_ptr] > min(self._left_child_ptr(parent_ptr), self._right_child_ptr(parent_ptr)):
+            child_ptr = min(self._left_child_ptr(parent_ptr), self._right_child_ptr(parent_ptr))
+            self.data[parent_ptr], self.data[child_ptr] = self.data[child_ptr], self.data[parent_ptr]
+            self._bubble_down(child_ptr)
 
 if __name__=="__main__":
 
@@ -61,5 +69,11 @@ if __name__=="__main__":
     heap.insert(3)
     heap.insert(5)
     heap.insert(1)
-
+    print heap.data
+    print " === Heap Insertion Completed === "
+    print heap.extract_min()
+    print heap.extract_min()
+    print heap.extract_min()
+    print heap.extract_min()
+    print " === Remaining Heap === "
     print heap.data
