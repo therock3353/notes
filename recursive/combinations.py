e5473c7b48abc962149009e23c9dad71e00ad27f
therock3353 <pushkarghayal@gmail.com>
2022-09-07 16:44:59 -0700

    combinations problem

--- recursive/combinations.py
+++ recursive/combinations.py
@@ -1,14 +1,20 @@
 """
+77. Combinations https://leetcode.com/problems/combinations/
 
     for i in range(1, 5):
         subsets(result+[i])
 
-        1                   2                   3                   4
- -----------------    ---------------    ----------------     --------------      ...
- |   |   |   |   |    |  |  |   |   |    |   |  |   |   |     |  |  |   |   |
- 1   2   3   4   5    1  2  3   4   5    1   2  3   4   5     1  2  3   4   5
+                    1                 2            3           4
+             ---------------      ----------    -------     -----      ...
+             |   |   |   |        |   |   |      |   |        |
+             2   3   4   5        3   4   5      4   5        5
+        3  4  5  |   5   (x)   4    5 |   (x)    5   (x)     (x)
+               ----                   5
+               4   5
 
     When k=2, we only go one level below and the recursion is cut at that level.
+
+    O(n) == ??
 
 """
 
@@ -32,3 +38,24 @@ if __name__=="__main__":
     #   [4, 5]
     # ]
     print subsets(n, k, 1, [], [])
+
+
+class Solution(object):
+    def recurse(self, remaining, k, result, results):
+        if k == 0:
+            results.append(result)
+            return
+
+        for i in range(len(remaining)):
+            self.recurse(remaining=remaining[i+1:],
+                         k=k-1,
+                         result=result+[remaining[i]],
+                         results=results)
+    def combine(self, n, k):
+        remaining = [i for i in range(1, n+1)]
+        results = []
+        self.recurse(remaining=remaining,
+                     k=k, result=[], results=results)
+        return results
+
+print Solution().combine(5, 3)
