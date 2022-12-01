bbcee08aab999b4984b8682b5a6a758fe4d1e8e1
therock3353 <pushkarghayal@gmail.com>
2022-11-30 17:20:36 -0800

    merge intervals variations
    merge intervals variations

--- sorting/merge_intervals.py
+++ sorting/merge_intervals.py
@@ -27,7 +27,6 @@ def merge_intervals(intervals):
         return intervals
     result = []
     intervals = sorted(intervals, key=itemgetter(0))
-    print intervals
     result.append(intervals[0])
     for i in range(1, len(intervals)):
         first = result.pop()
@@ -41,7 +40,71 @@ def merge_intervals(intervals):
             result.append(second)
     return result
 
-A = [[5, 10], [16, 20], [8, 13], [14, 20]]
-A = [[5, 8], [5, 8]]
-A = [[10, 20], [30, 40]]
-print merge_intervals(A)
+# A = [[5, 10], [16, 20], [8, 13], [14, 20]]
+# A = [[5, 8], [5, 8]]
+# A = [[10, 20], [30, 40]]
+# print merge_intervals(A)
+
+
+'''
+252. Meeting Rooms
+Given an array of meeting time intervals where intervals[i] = [starti, endi], 
+determine if a person could attend all meetings.
+
+'''
+
+def canAttendMeetings(intervals):
+    if not intervals:
+        return False
+    if len(intervals) == 1:
+        return True
+    i = 0
+    intervals = sorted(intervals, key=itemgetter(0))
+    while i < len(intervals)-1:
+        if intervals[i+1][0] < intervals[i][1]:
+            return False
+        i += 1
+    return True
+
+# A = [[5, 10], [16, 20], [8, 13], [14, 20]]
+# A = [[5, 8], [5, 8]]
+# A = [[10, 20], [30, 40]]
+# print canAttendMeetings(A)
+
+
+'''
+253. Meeting Rooms II
+Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], 
+return the minimum number of conference rooms required.
+https://leetcode.com/problems/meeting-rooms-ii/
+'''
+import heapq
+def minMeetingRooms(intervals):
+    if not intervals:
+        return -1
+    max_num_rooms = 1
+    curr_num_rooms = 1
+    ending_times = []
+    intervals = sorted(intervals, key=itemgetter(0))
+    heapq.heappush(ending_times, intervals[0][1])
+    for i in range(1, len(intervals)):
+        if ending_times:
+            curr_end_time = heapq.heappop(ending_times)
+        else:
+            heapq.heappush(ending_times, intervals[i][1])
+            curr_num_rooms = 1
+            continue
+        if intervals[i][0] < curr_end_time:
+            curr_num_rooms += 1
+            heapq.heappush(ending_times, curr_end_time)
+            heapq.heappush(ending_times, intervals[i][1])
+        else:
+            curr_num_rooms = max(1, curr_num_rooms)
+            heapq.heappush(ending_times, intervals[i][1])
+        if curr_num_rooms > max_num_rooms:
+            max_num_rooms = curr_num_rooms
+    return max_num_rooms
+
+A = [[1,8],[6,20],[9,16],[13,17]]
+#A = [[1, 4], [5, 8], [6, 9]]
+print minMeetingRooms(A)
